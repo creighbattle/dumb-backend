075c7cf8a3b8ad224dcdb2bfb39db4c676713845
Creigh Battle <creighbattle@Creighs-MacBook-Pro.local>
2023-11-26 13:09:47 -0500

    bug fix

--- index.js
+++ index.js
@@ -46,11 +46,11 @@ app.get("/create-thread-message", async (req, res) => {
 app.post("/ask", async (req, res) => {
   try {
     const question = req.body.question;
-    const threadId = "thread_yMpF4nEqCXsl4QOpY0ntYxsX";
+    const threadId = "thread_JC6OCRI9f5dYCAqT8qgBlBrK";
 
     const message = await openai.beta.threads.messages.create(threadId, {
       role: "user",
-      content: question,
+      content: "Look at your instructions. " + question,
     });
     res.json({ data: message });
   } catch (error) {
@@ -61,7 +61,7 @@ app.post("/ask", async (req, res) => {
 
 app.get("/run-assistant", async (req, res) => {
   try {
-    const threadId = "thread_yMpF4nEqCXsl4QOpY0ntYxsX";
+    const threadId = "thread_JC6OCRI9f5dYCAqT8qgBlBrK";
 
     const run = await openai.beta.threads.runs.create(threadId, {
       assistant_id: "asst_pSk3Endw49XCedzJyt0GYVBE",
@@ -76,7 +76,7 @@ app.get("/run-assistant", async (req, res) => {
 
 app.post("/check-run", async (req, res) => {
   try {
-    const threadId = "thread_yMpF4nEqCXsl4QOpY0ntYxsX";
+    const threadId = "thread_JC6OCRI9f5dYCAqT8qgBlBrK";
     //const runId = "run_gFnxlt1NZOd4yv5NC1iKc5wl";
     const runId = req.body.runId;
 
@@ -90,7 +90,7 @@ app.post("/check-run", async (req, res) => {
 
 app.get("/get-message", async (req, res) => {
   try {
-    const threadId = "thread_yMpF4nEqCXsl4QOpY0ntYxsX";
+    const threadId = "thread_JC6OCRI9f5dYCAqT8qgBlBrK";
 
     // const messages = await openai.beta.threads.messages.list(threadId);
     // console.log(messages.data[0]);
